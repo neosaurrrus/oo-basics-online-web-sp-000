f6f830707e2e0f332cca39c6399542824e6a2ea1
Lukie Kang <lukiek+f@gmail.com>
2019-06-03 21:47:03 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -20,11 +20,18 @@ end
 def size
   @size
 end
+
 def material=(material)
   @material = material
 end
 def material
   @material
+end
+def condition=(condition)
+  @condition = condition
+end
+def condition
+  @condition
 end
 
 
